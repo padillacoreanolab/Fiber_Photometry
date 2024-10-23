a397055c903deafb29cff996e51b4d9f36443d46
Nayla Jimenez <naylajimenez@ufl.edu>
2024-10-23 12:19:06 -0400

    trial

--- trial_class.py
+++ trial_class.py
@@ -2,10 +2,17 @@ import numpy as np
 import pandas as pd
 import tdt
 import matplotlib.pyplot as plt
+import os
 
 class Trial:
-    def _init_(self, trial_path):
+    def __init__(self, trial_path):
         tdtdata = tdt.read_block(trial_path)
         self.streams = {}
-        self.behaviors = {key: value for key, value in tdt_data.epocs.items() if key not in ['Cam1', 'Tick']}
+        self.behaviors = {key: value for key, value in tdtdata.epocs.items() if key not in ['Cam1', 'Tick']}
+        
+        self.subject_name = os.path.basename(trial_path).split('-')[0]
+
+        self.fs = tdtdata.streams["_465A"].fs
+        self.timestamps = np.arange(len(tdtdata.streams['_465A'].data)) / self.fs
+
 
